e064ec9bb77cee83be43c6ce71fe8c265d59b473
Nam Nguyen <nnguyen17@hawk.iit.edu>
2022-09-12 21:29:29 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
+
+
+
+
+
---
+++ ConsoleApp1/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(async endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello world!!");
+                });
+            });
+        }
+    }
+}
